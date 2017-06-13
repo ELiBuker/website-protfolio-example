dab991502b6ba4dfcd98a31fbe9755f5dd4f40df
ACE User <aceuser@e140-s23.lab.lagcc>
2017-06-12 21:17:10 -0400

    made Toggle Nave Bar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log("this works");
+var topNavList = document.querySelector('#top-nav-list');
+var topNavToggle = document.querySelector('#top-nav-toggle');
+var navCollapsed = true;
+
+topNavToggle.addEventListener('click', function(){
+	// console.log('string workin on line 5');
+	if(navCollapsed === true){
+		topNavList.classList.remove("nav-list-collapsed");
+		topNavToggle.innerHTML = '<i class="fa fa-bars" aria-hidden="true"></i>';
+		navCollapsed = false;
+	}else {
+		topNavList.classList.add("nav-list-collapsed");
+		topNavToggle.innerHTML = '<i class="fa fa-times" aria-hidden="true"></i>';
+		navCollapsed = true;
+	}
+});
